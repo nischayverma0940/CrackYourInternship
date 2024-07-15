ee8f3fadebe944cdfa545b51a9694fc290dfca3f
nischayverma0940 <113400888+nischayverma0940@users.noreply.github.com>
2024-07-15 13:09:36 +0530

    Update buyAndSellStock.cpp

--- Array/buyAndSellStock.cpp
+++ Array/buyAndSellStock.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int minBuy = prices[0];
+        for(int i = 1; i < prices.size(); i++){
+            profit = max(prices[i] - minBuy, profit);
+            if(prices[i] < minBuy) minBuy = prices[i];
+        }
+        return profit;
+    }
+};
+//Leetcode 121
